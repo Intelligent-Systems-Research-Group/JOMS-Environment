0c2c739f558ca1b1c00008db4b1669ffdf0e6db4
zesa0001 <samuel.zeitvogel@hs-karlsruhe.de>
2020-10-01 23:12:15 +0200

    initial commit

---
+++ scripts/build_docker.sh
@@ -0,0 +1,11 @@
+docker build -t joms/opensubdiv:latest --compress opensubdiv
+docker build -t joms/cxxopts:latest --compress cxxopts
+docker build -t joms/embree:latest --compress embree
+docker build -t joms/fbxsdk:latest --compress fbx
+docker build -t joms/clang:latest --compress clang
+docker build -t joms/terra:latest --compress terra
+docker build -t joms/nanoflann:latest --compress nanoflann
+docker build -t joms/json:latest --compress json
+docker build -t joms/optlang:latest --compress Optlang
+docker build -t joms/trainer:latest --compress human-model-trainer
+docker build -t joms/openpose:latest --compress openpose/openpose-docker/
