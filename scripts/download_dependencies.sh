5a865740842122b96bda0e3f60085fcda55b519b
zesa0001 <samuel.zeitvogel@hs-karlsruhe.de>
2020-10-05 02:35:30 +0200

    add output folder and fixed dependencies

---
+++ scripts/download_dependencies.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/Intelligent-Systems-Research-Group/JOMS
+git clone https://github.com/llvm/llvm-project clang/llvm-project
+git --git-dir clang/llvm-project/.git checkout release/7.x
+git clone https://github.com/Intelligent-Systems-Research-Group/fbx
+git clone https://github.com/Intelligent-Systems-Research-Group/opensubdiv
+git clone https://github.com/Intelligent-Systems-Research-Group/json
+git clone https://github.com/Intelligent-Systems-Research-Group/nanoflann
+git clone https://github.com/Intelligent-Systems-Research-Group/embree
+git clone https://github.com/Intelligent-Systems-Research-Group/cxxopts
+git clone https://github.com/Intelligent-Systems-Research-Group/terra
+git clone https://github.com/Intelligent-Systems-Research-Group/Optlang
