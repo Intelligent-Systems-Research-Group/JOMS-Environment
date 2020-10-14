89765733b0b3700dc24e7b0eb502129d908c21fb
zesa0001 <samuel.zeitvogel@hs-karlsruhe.de>
2020-10-14 21:41:33 +0200

    fixed download script

--- scripts/download_dependencies.sh
+++ scripts/download_dependencies.sh
@@ -1,6 +1,5 @@
 git clone https://github.com/Intelligent-Systems-Research-Group/JOMS
 git clone https://github.com/llvm/llvm-project clang/llvm-project
-git --git-dir clang/llvm-project/.git checkout release/7.x
 git clone https://github.com/Intelligent-Systems-Research-Group/fbx
 git clone https://github.com/Intelligent-Systems-Research-Group/opensubdiv
 git clone https://github.com/Intelligent-Systems-Research-Group/json
