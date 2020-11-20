b65a85fdb9b89bf1e5e84ecba6c786659141b5d7
Intelligent-Systems-Research-Group <68399436+Intelligent-Systems-Research-Group@users.noreply.github.com>
2020-11-20 19:44:01 +0100

    fixed missing clang docker build command

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -4,6 +4,7 @@ docker build -t joms/opensubdiv:latest --compress opensubdiv
 docker build -t joms/cxxopts:latest --compress cxxopts
 docker build -t joms/embree:latest --compress embree
 docker build -t joms/fbxsdk:latest --compress fbx
+docker build -t joms/clang:latest --compress clang
 docker build -t joms/terra:latest --compress terra
 docker build -t joms/nanoflann:latest --compress nanoflann
 docker build -t joms/json:latest --compress json
